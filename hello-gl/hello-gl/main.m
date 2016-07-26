32b94ff9c93007ae4afda01d50d9ccc0d87dde4e
Kyle Stewart <kylestew@gmail.com>
2016-07-25 20:10:23 -0700

    Learned you cannot use a command line OSX app for this stuff

--- hello-gl/hello-gl/main.m
+++ hello-gl/hello-gl/main.m
@@ -5,8 +5,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "util.h"
 
-static int make_resources(void) {
+
+static struct {
+    GLuint vertexBuffer;
+    GLuint elementBuffer;
+    GLuint textures[2];
+} gResources;
+
+static const GLfloat VERTEX_BUFFER_DATA[] = {
+    -1.0f, -1.0f,
+    1.0f, -1.0f,
+    -1.0f, 1.0f,
+    1.0f, 1.0f};
+static const GLushort ELEMENT_BUFFER_DATA[] = {
+    0, 1, 2, 3};
+
+#pragma mark - OpenGL Creation Helpers
+static GLuint makeBuffer(GLenum target, const void *buffer_data, GLsizei buffer_size) {
+    GLuint buffer;
+    // generate
+    // bind
+    // fill
+    glGenBuffers(1, &buffer);
+    glBindBuffer(target, buffer);
+    glBufferData(target, buffer_size, buffer_data, GL_STATIC_DRAW);
+    return buffer;
+}
+
+static GLuint makeTexture(const char *filename) {
+    int width, height;
+    void *pixels = read_tga(filename, &width, &height);
+    GLuint texture;
+
+    glGenTextures(1, &texture);
+
+    return texture;
+}
+
+
+static int makeResources(void) {
+    gResources.vertexBuffer = makeBuffer(GL_ARRAY_BUFFER, VERTEX_BUFFER_DATA, sizeof(VERTEX_BUFFER_DATA));
+    gResources.vertexBuffer = makeBuffer(GL_ELEMENT_ARRAY_BUFFER, ELEMENT_BUFFER_DATA, sizeof(ELEMENT_BUFFER_DATA));
+
+    gResources.textures[0] = makeTexture("hello1.tga");
+    gResources.textures[1] = makeTexture("hello2.tga");
+
     return 1;
 }
 
@@ -28,7 +73,7 @@ int main(int argc, char **argv) {
     glutIdleFunc(&update_fade_factor);
     glutDisplayFunc(&render);
 
-    if (!make_resources()) {
+    if (!makeResources()) {
         fprintf(stderr, "Failed to load resources\n");
         return 1;
     }
