a345870c9051181588c62f2b8902c9dcfe9a8ed8
Kyle Stewart <kylestew@gmail.com>
2016-08-06 18:12:57 -0700

    ...

---
+++ flag/flag/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  flag
+//
+//  Created by Kyle Stewart on 7/26/16.
+//  Copyright © 2016 Kyle Stewart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
